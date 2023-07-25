43d75d6575cf758bed320811d2e45eb901814ac4
lixel_ <133605174+lixelv@users.noreply.github.com>
2023-07-25 18:02:35 +0300

    Add files via upload

--- run.py
+++ run.py
@@ -2,30 +2,32 @@ import streamlit as st
 import sqlite3
 import pandas as pd
 
-st.set_page_config(
-    page_title="Европоиск",
-    page_icon="💾",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
 
+if 'session_created' not in st.session_state:
+    st.set_page_config(
+        page_title="Европоиск",
+        page_icon="💾",
+        layout="wide",
+        initial_sidebar_state="expanded"
+    )
 
-# Скрываем меню и подвал с помощью CSS
-hide_streamlit_style = """
-<style>
-#MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
-header {visibility: hidden;}
-</style>
-"""
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
-# Создаем подключение к SQLite
-conn = sqlite3.connect('data.db')
-cursor = conn.cursor()
+    # Создаем подключение к SQLite
+    st.session_state['session_created'] = True
 
 # Пользовательский ввод в боковой панели
+md = ''
+conn = sqlite3.connect('data.db')
+cursor = conn.cursor()
 user_input = st.sidebar.text_input("Введите поисковый запрос:")
+
+hide_streamlit_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    </style>
+    """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 if user_input:
     # Формируем и выполняем SQL-запрос поиска
@@ -37,13 +39,24 @@ else:
 results = cursor.fetchall()
 
 # Получаем названия колонок из cursor.description
-column_names = [desc[0] for desc in cursor.description]
+column_names = [desc[0] for desc in cursor.description[:2]] + ['Клапан', 'Распылитель', 'Стакан']
 
 # Если есть результаты, отображаем их в виде DataFrame
-if results:
+if len(results) <= 5:
+    for i in results:
+        df = pd.DataFrame([i], columns=column_names)
+        # Используйте метод any() для проверки, есть ли хотя бы одно True (т.е. значение 0) в каждой строке
+        zero_rows = df.columns[(df == 0).any()].tolist()
+        md += f'### Отсутствует запчасть у {i[1].split(" ")[-1]}.\n' if zero_rows else ''
+        # Выведите названия строк
+        for row in zero_rows:
+             md += f'- <font size=6 color="#FF0000"><b>{row}</b></font>\n'
+        # st.dataframe(df, hide_index=True, use_container_width=True)
+
+    st.markdown(md if md else "# Форсунка в полном составе!", unsafe_allow_html=True)
+elif results:
     df = pd.DataFrame(results, columns=column_names)
 
-    st.data_editor(df, hide_index=True)
-    #  st.markdown(df.to_html(index=False), unsafe_allow_html=True)
+    st.dataframe(df, hide_index=True, use_container_width=True)
 else:
-    st.write("Результаты не найдены.")
+    st.markdown("# Ничего не найдено")
--- test.py
+++ test.py
@@ -1,11 +1,48 @@
 import pandas as pd
-from sqlalchemy import create_engine
+import time, random
+import sqlite3
+from prettytable import PrettyTable
 
-# Загрузите данные из Excel в DataFrame
-df = pd.read_excel('data.xlsx')  # Укажите здесь свой путь к файлу
+conn = sqlite3.connect('data.db')
+cur = conn.cursor()
 
-# Создайте подключение к базе данных
-engine = create_engine('sqlite:///Z:/GitHub/europoisk/data.db')
+cur.execute('SELECT MAX(id) FROM data')
+for i in range(1, cur.fetchone()[0]+1):
+	for _ in range(random.choice(range(0, 30))):
+		cur.execute('INSERT INTO data (name, clapan, raspil, stakan, jumper, cupper_ring) SET (SELECT name FROM data WHERE id = ?), ?, ?, ?, ?, ? WHERE id=?', (i, random.choice([False, True, True]), random.choice([False, True, True]), random.choice([False, True, True]), random.choice([False, True, True]), random.choice([False, True, True]), i))
+conn.commit()
+# def compare_pd(df_1, df_2):
 
-# Запишите данные из DataFrame в таблицу в базе данных
-df.to_sql('data', engine, if_exists='replace', index=False)  # Заменил 'table_name' на 'data', поскольку вы указали, что таблица называется 'data'
+# 	df_1.set_index('id', inplace=True)
+# 	df_2.set_index('id', inplace=True)
+	
+# 	diff_df = df_1.compare(df_2)
+
+# 	diff_df = diff_df.xs('self', axis=1, level=1)
+
+# 	# Преобразуем DataFrame в формат "длинный", где каждая строка это пара "id" и "значение".
+# 	melted_df = diff_df.reset_index().melt(id_vars='id', var_name='column', value_name='data')
+
+# 	# Преобразуем преобразованный DataFrame в список словарей.
+# 	return melted_df.to_dict('records')
+
+# conn = sqlite3.connect('data.db')
+# cursor = conn.cursor()
+# cursor.execute("UPDATE data SET name='Тmaы лох!' WHERE id=132")
+# conn.commit()
+# cursor.execute('SELECT * FROM data')
+
+# conn_1 = sqlite3.connect('data_1.db')
+# cursor_1 = conn_1.cursor()
+# cursor_1.execute('SELECT * FROM data')
+
+# # Тестирование функции
+# df_1 = pd.DataFrame(cursor.fetchall(), columns=[desc[0] for desc in cursor.description])
+# df_2 = pd.DataFrame(cursor_1.fetchall(), columns=[desc[0] for desc in cursor_1.description])
+
+# # Преобразуем преобразованный DataFrame в список словарей.
+# result = compare_pd(df_1, df_2)
+
+# print(result)
+# zero_rows = df_1.columns[(df_1 == 0).any()].tolist()
+# print(zero_rows)
